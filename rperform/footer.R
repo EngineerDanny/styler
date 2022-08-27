679f855461270b495db354573f473608862e3f36
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-08-26 18:26:45 -0700

    footer update

--- rperform/footer.R
+++ rperform/footer.R
@@ -1,9 +1,5 @@
 # You can modify the PR comment footer here. You can use github markdown e.g.
 # emojis like :tada:.
 glue::glue(
-<<<<<<< HEAD
   ""
-=======
-  "\n--------------------------------------------------\n"
->>>>>>> 5f9748c064c81ca8a2039e6ca88a02dbb28c7c9e
 )
