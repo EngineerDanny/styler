6b83332be1cd507b87cb4e87e3cd4cafe9baea0f
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-08-26 18:19:42 -0700

    update footer

--- rperform/footer.R
+++ rperform/footer.R
@@ -1,7 +1,5 @@
-
 # You can modify the PR comment footer here. You can use github markdown e.g.
 # emojis like :tada:.
-link <- "https://github.com/analyticalmonk/Rperform#readme"
 glue::glue(
-  "\n--------------------------------------------------\n"
+  ""
 )
