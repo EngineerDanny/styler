6d4958ac238bfa2ba201f3ec2c702d58aa22a996
EngineerDanny <agyapongdaniel7777@gmail.com>
2022-08-27 17:11:32 -0700

    update script

--- rperform/script.R
+++ rperform/script.R
@@ -4,7 +4,7 @@
 ## TEST 1
 Rperform::plot_metrics(
   test_path = "tests/testthat/test-rmd.R",
-  metric = "time", num_commits = 10,
+  metric = "time", num_commits = 100,
   save_data = TRUE,
   save_plots = TRUE,
   total_width_in = 15
@@ -13,7 +13,7 @@ Rperform::plot_metrics(
 ## TEST 2
 Rperform::plot_metrics(
   test_path = "tests/testthat/test-escaping.R",
-  metric = "time", num_commits = 10,
+  metric = "time", num_commits = 100,
   save_data = TRUE,
   save_plots = TRUE
 )
